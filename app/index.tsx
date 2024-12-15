6de0bbdd0786393e097d56216b33e2baefb62185
John Adibe <johnadibe450@gmail.com>
2024-12-15 12:35:55 +0100

    update: expo package update

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { FlatList, StyleSheet, Text, View } from "react-native";
-import { Href, Link } from "expo-router";
+import { Link } from "expo-router";
 import { COLORS } from "@/constants/colors";
 import { RN_TIPS } from "@/constants/app";
 
